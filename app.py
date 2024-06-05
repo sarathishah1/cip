4c4e47e14a8af424fd9869a7de199d94fdd722e6
TocaSoft <tocasoft@yahoo.com>
2024-06-04 21:58:05 -0400

    modified:   app.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, send_from_directory
+
 app = Flask(__name__)
 
+# Ruta para devolver el JSON
+@app.route('/get_json')
+def get_json():
+    data = {
+        "key1": "value1",
+        "key2": "value2"
+    }
+    return jsonify(data)
+
+# Ruta para servir un fichero JSON estático
 @app.route('/')
-def hello_world():
-    return 'Hello, ok Viva Cuba Libre Joseito a!ok'
+def index():
+    return send_from_directory('static', 'example2.json')
+
