cb6ff02c1fccc35f7152fdf7c77a40a33b672586
Nadal <94201433+NadalBQ@users.noreply.github.com>
2024-06-16 19:32:15 +0200

    Update app.py
    Same as in the javascript one, i want this to connect to the model

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.json
+    pais = int(data['pais'])
+    renta = int(data['renta'])
+    result = pais + renta  # Your processing logic here
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
