90f77a559cb58a3d58d45fae702c70345f9dc2f2
OBL <obl@example.com>
2024-06-20 11:00:37 +0300

    change hoods + repair bug

--- address.py
+++ address.py
@@ -1,10 +1,11 @@
 from typing import Dict, Any
-from geopy.geocoders import Nominatim
+from geopy.geocoders import Nominatim, Photon
 from shapely import Point
 import pickle
 
-UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
 ISRAEL = 'ישראל'
+DATA_FILENAME = 'data/hoods_haifa.pkl'
 
 
 class Address:
@@ -15,6 +16,7 @@ class Address:
         self.country = country
         self.geometry: Point = None
         self.area_id: int = None
+        self.area_name: str = None
         self.area_data = None
 
     def _get_address_point(self) -> Point:
@@ -22,7 +24,7 @@ class Address:
         takes an Israeli address and queries the free Nominatim geocoding API to get it's location.
         spelling should be precise.
         """
-        coder = Nominatim(user_agent=UA)
+        coder = Photon(user_agent="abcd")
 
         addr = self.street + ' ' + str(self.building_num)
         full_addr = addr + ', ' + self.city + ', ' + self.country
@@ -37,13 +39,14 @@ class Address:
             raise ValueError('Address geocoding failed')
 
     def _point_to_area_id(self) -> int:
-        with open('data/geodata_pickle', 'rb') as f:
+        with open(DATA_FILENAME, 'rb') as f:
             gdf = pickle.load(f)
 
         area = gdf[gdf.contains(self.geometry)]
         if not area.empty:
             self.area_data = {k: list(v.values())[0] for k, v in area.to_dict().items()}
-            self.area_id = self.area_data['YISHUV_STAT_2022']
+            self.area_id = self.area_data['ID']
+            self.area_name = self.area_data['SchName']
             return self.area_id
 
         raise ValueError('No area found for the point')
@@ -51,3 +54,10 @@ class Address:
     def get_area_id(self) -> int:
         self._get_address_point()
         return self._point_to_area_id()
+    
+
+if __name__ == "__main__":
+    addr = Address(5, 'אז"ר', 'חיפה')
+    addr.get_area_id()
+    print(addr.area_id)
+    print(addr.area_name)
