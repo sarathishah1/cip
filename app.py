59d16dce2786b00cc3b2b547d9abaf8181ab29e7
Aarish Mohmmad <99970799+Aarishmd@users.noreply.github.com>
2024-07-15 09:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is Aarish Thanks'
+    return 'Hello, World! This is Aarish Thanks '
 if __name__ == "__main__":
     app.run()
